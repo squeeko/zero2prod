69c2a20efd98b3552dfd3c0a268ea35f0bd74c95
squeeko <squeeko@users.noreply.github.com>
2022-10-22 16:56:07 -0700

    tokio async and testing

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+// async fn greet(req: HttpRequest) -> impl Responder {
+//     let name = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello {}!", &name)
+// }
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            // .route("/", web::get().to(greet))
+            // .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
