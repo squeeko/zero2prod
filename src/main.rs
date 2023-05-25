461bc098b66702b6d4a7c573cdc34ee61259c8c3
Jermaine Allgood <jermaine.allgood@windriver.com>
2023-05-24 17:45:53 -0700

    greet handler

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-use std::net::TcpListener;
+use actix_web::{web, App, HttpRequest, Responder, HttpServer};
 
-use zero2prod::run;
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+
+    
 }
